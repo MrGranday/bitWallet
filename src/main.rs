3e28db450dba19cd8dcacfac3e11911445e31fad
Osman Ghani Granday <osmamghani009009@gmail.com>
2025-10-04 03:55:54 +0500

    worked with tokio

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
+use dotenv::dotenv;
+use std::env;
+
 async fn the_main() {
     println!("hello osman ");
 }
 #[tokio::main]
 async fn main() {
+    dotenv().ok();
+
+    let mongo = env::var("MONGODB_URL");
+
+    match mongo {
+        Ok(val) => println!("MONGODB_URL: {:?}", val),
+        Err(e) => println!("Error MONGODB_URL: {:?}", e),
+    }
     the_main().await;
     println!("Hello, world!");
 }
